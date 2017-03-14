61c7776cf09054e401ffff077464e171224ae74f
wanwa973 <wanwa973@student.otago.ac.nz>
2017-03-14 18:00:51 +1300

    Uses PUT to replace a resource with a new version

--- lab03/src/resources/ProductResource.java
+++ lab03/src/resources/ProductResource.java
@@ -3,7 +3,7 @@ package resources;
 import dao.ProductDAO;
 import domain.Product;
 import javax.ws.rs.*;
-
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -11,22 +11,41 @@ import javax.ws.rs.*;
  */
 @Path("/products/{productID}")
 public class ProductResource {
-	private final ProductDAO item = new ProductDAO();
+	private final ProductDAO itemDAO = new ProductDAO();
 	private final String productID;
 	private final Product product;
 
 	
 	public ProductResource(@PathParam("productID") String  productID) {
 		this.productID = productID;
-		this.product = this.item.getByID(productID);
+		this.product = this.itemDAO.getByID(productID);
 		
-		if(!item.exists(productID)) {
+		if(!itemDAO.exists(productID)) {
 			throw new NotFoundException("Sorry, product ID is not found.");
 		} 
 	}	 
 
 	@GET
 	public Product getProduct() {
-		return product;
+		return this.product;
 	}
+	
+	@DELETE
+	public void deleteProduct() {
+		itemDAO.delete(product);
+	}
+	
+	@PUT
+	public Response updateProduct(Product updatedProduct) {
+		if(this.productID.equals(updatedProduct.getId())) {
+			itemDAO.updateProduct(this.productID, updatedProduct);
+			return Response.noContent().build();
+		} else {
+			return Response
+					  .status(Response.Status.CONFLICT)
+					  .entity("IDs don't match")
+					  .build();
+		}
+	}
+	
 }
