e1b75614e66c2ea27310d5608c4a5c7eb320071e
wanwa973 <wanwa973@student.otago.ac.nz>
2017-03-21 17:48:43 +1300

    Displays the selected product

--- HTML5Application/public_html/scripts/inventory.js
+++ HTML5Application/public_html/scripts/inventory.js
@@ -5,30 +5,31 @@ var module = angular.module('InventoryApp', ['ngResource']);
 
 // create the controller and inject the Angular features that we need
 module.controller('InventoryController', function ($scope, $resource) {
-	$scope.summaries = {
-  "product_summary" : [
-    {
-      "id" : "FD1234",
-      "name" : "Freckled Doohicky",
-      "uri" : "http://localhost:8081/products/FD1234"
-    },
-    {
-      "id" : "WF3412",
-      "name" : "Wuzzy Fidget",
-      "uri" : "http://localhost:8081/products/WF3412"
-    },
-    {
-      "id" : "FW4321",
-      "name" : "Fuzzy Widget",
-      "uri" : "http://localhost:8081/products/FW4321"
-    }
-  ]
-}
-			  
+	//resource stub to work with inventory and summaries 
+	/* The extra slashes are necessary because AngularJS uses colons as 
+	 * placeholders for path variables. We need to escape the colon in front 
+	 * of the port to stop AngularJS from trying 
+	 * to use the port as a variable. 
+	 */
+	var inventoryResource = $resource('http://localhost\\:8081/products');
+	
+	//gets the summaries from the service using the resource stub
+	$scope.summaries = inventoryResource.get();
 
    
   
    $scope.displaySelectedProduct = function() {
+		//is there a valid summary selected?
+		if($scope.selected !== null) {
+			//gets the URI of the selected summary
+			var uri = $scope.selected.uri;
+		
+		  //uses URI to GET the product and put it in the model
+		  $scope.product = $resource(uri).get();
+		} else {
+			//if not, clear the existing product
+			$scope.product = null;
+		}
 
    };
 
